ca2c6730da92b01471e89c0ccaef79fdad18298a
Vadim A. Nikulin <accs@vnik.me>
2020-01-17 08:31:20 +0000

    Move all possible variables to global.

--- timus/2109/2109.cpp
+++ timus/2109/2109.cpp
@@ -1,20 +1,14 @@
+#include <algorithm>
 #include <ios>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 
-struct TGraph {
-    std::vector<std::vector<int>> Parents;
-    std::vector<int> Depth;
-};
-
-struct TIntervalTree {
-    int Pow2 = 0;
-    std::vector<int> Tree;
-};
-
-TGraph graph;
-TIntervalTree tree;
+std::vector<std::vector<int>> Parents;
+std::vector<int> Depth;
+int Pow2 = 0;
+std::vector<int> Tree;
 
 
 int GreaterOrEqualPow2(int n) {
@@ -34,35 +28,35 @@ void ReadGraph() {
         links[b - 1].push_back(a - 1);
     }
     std::vector<int> queue;
-    queue.reserve(n - 1);
+    queue.reserve(n);
     queue.push_back(0);
-    graph.Parents.resize(n);
-    int pow2 = GreaterOrEqualPow2(n);
+    Parents.resize(n);
+    Pow2 = GreaterOrEqualPow2(n);
     for (int i = 0; i < n; ++i)
-        graph.Parents[i].reserve(pow2);
-    graph.Depth.resize(n);
-    graph.Parents[0].clear();
-    graph.Depth[0] = 0;
+        Parents[i].reserve(Pow2 + 1);
+    Depth.resize(n);
+    Depth[0] = 0;
     for (size_t i = 0; i < queue.size(); ++i) {
         int current = queue[i];
         for (int child : links[current]) {
             if (child == 0)
                 continue;
-            if (!graph.Parents[child].empty())
+            if (!Parents[child].empty())
                 continue;
-            graph.Parents[child].push_back(current);
-            graph.Depth[child] = graph.Depth[current] + 1;
+            Parents[child].push_back(current);
+            Depth[child] = Depth[current] + 1;
             queue.push_back(child);
         }
     }
     for (int current : queue) {
-        for (size_t level = 0; level < graph.Parents[current].size(); ++level) {
-            if (level + 1 != graph.Parents[current].size())
+        auto &currentParents = Parents[current];
+        for (size_t level = 0; level < currentParents.size(); ++level) {
+            if (level + 1 != currentParents.size())
                 throw 1;
-            int parent = graph.Parents[current][level];
-            if (level >= graph.Parents[parent].size())
+            int parent = currentParents[level];
+            if (level >= Parents[parent].size())
                 break;
-            graph.Parents[current].push_back(graph.Parents[parent][level]);
+            currentParents.push_back(Parents[parent][level]);
         }
     }
 }
@@ -71,7 +65,7 @@ int GetParent(int vertex, int level) {
     int pow2 = 0;
     while (level != 0) {
         if (level % 2 == 1)
-            vertex = graph.Parents[vertex][pow2];
+            vertex = Parents[vertex][pow2];
         level >>= 1;
         ++pow2;
     }
@@ -79,10 +73,12 @@ int GetParent(int vertex, int level) {
 }
 
 int FindCommonParent(int a, int b) {
-    int l = 0, r = graph.Parents[a].size() - 1, result = -1;
+    const auto &pa = Parents[a];
+    const auto &pb = Parents[b];
+    int l = 0, r = pa.size() - 1, result = -1;
     while (l <= r) {
         int m = (l + r) / 2;
-        if (graph.Parents[a][m] != graph.Parents[b][m]) {
+        if (pa[m] != pb[m]) {
             result = m;
             l = m + 1;
         } else {
@@ -93,7 +89,7 @@ int FindCommonParent(int a, int b) {
 }
 
 int CommonParent(int a, int b) {
-    int diff = graph.Depth[a] - graph.Depth[b];
+    int diff = Depth[a] - Depth[b];
     if (diff != 0) {
         if (diff > 0)
             a = GetParent(a, diff);
@@ -105,30 +101,29 @@ int CommonParent(int a, int b) {
     for (;;) {
         int level = FindCommonParent(a, b);
         if (level == -1)
-            return graph.Parents[a][0];
-        a = graph.Parents[a][level];
-        b = graph.Parents[b][level];
+            return Parents[a][0];
+        a = Parents[a][level];
+        b = Parents[b][level];
     }
 }
 
 void BuildIntervalTree() {
-    tree.Pow2 = GreaterOrEqualPow2(graph.Depth.size());
-    tree.Tree.resize((1 << (tree.Pow2 + 1)) - 1, -1);
-    int levelBegin = (1 << tree.Pow2) - 1, levelSize = (1 << tree.Pow2);
-    for (int i = 0; i < graph.Parents.size(); ++i)
-        tree.Tree[levelBegin + i] = i;
+    Tree.resize((1 << (Pow2 + 1)) - 1, -1);
+    int levelBegin = (1 << Pow2) - 1, levelSize = (1 << Pow2);
+    for (int i = 0; i < Parents.size(); ++i)
+        Tree[levelBegin + i] = i;
     while(levelBegin != 0) {
         levelBegin = (levelBegin - 1) / 2;
         levelSize >>= 1;
-        for (int i = 0; i < levelSize; ++i) {
-            int idx1 = (levelBegin + i) * 2 + 1, idx2 = (levelBegin + i) * 2 + 2;
-            int ans1 = tree.Tree[idx1], ans2 = tree.Tree[idx2];
+        for (int i = levelBegin; i < levelBegin + levelSize; ++i) {
+            int idx1 = i * 2 + 1, idx2 = i * 2 + 2;
+            int ans1 = Tree[idx1], ans2 = Tree[idx2];
             if (ans2 == -1)
-                tree.Tree[levelBegin + i] = ans1;
+                Tree[i] = ans1;
             else if (ans1 == -1)
-                tree.Tree[levelBegin + i] = ans2;
+                Tree[i] = ans2;
             else
-                tree.Tree[levelBegin + i] = CommonParent(ans1, ans2);
+                Tree[i] = CommonParent(ans1, ans2);
         }
     }
 }
@@ -138,7 +133,7 @@ int DoCommonParentForInterval(int index, int intervalBegin, int intervalSize, in
         return -1;
     if (b < intervalBegin)
         return -1;
-    int ansRoot = tree.Tree[index];
+    int ansRoot = Tree[index];
     if (ansRoot == -1)
         return -1;
     if (intervalBegin >= a && intervalBegin + intervalSize - 1 <= b)
@@ -153,7 +148,7 @@ int DoCommonParentForInterval(int index, int intervalBegin, int intervalSize, in
 }
 
 int CommonParentForInterval(int a, int b) {
-    return DoCommonParentForInterval(0, 0, (1 << tree.Pow2), a, b);
+    return DoCommonParentForInterval(0, 0, (1 << Pow2), a, b);
 }
 
 void ProcessQueries() {
@@ -161,9 +156,7 @@ void ProcessQueries() {
     for (std::cin >> q; q > 0; --q) {
         int a, b;
         std::cin >> a >> b;
-        --a;
-        --b;
-        std::cout << CommonParentForInterval(a, b) + 1 << '\n';
+        std::cout << CommonParentForInterval(a - 1, b - 1) + 1 << '\n';
     }
 }
 
